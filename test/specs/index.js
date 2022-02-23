e049c6a4bba696c0a75ab45b1b5240e96fdd0a37
Leonard Martin <leonard.martin@gmail.com>
2022-02-23 16:39:59 +0000

    Add end to end integration test

--- test/specs/index.js
+++ test/specs/index.js
@@ -1,3 +1,5 @@
+const assert = require('assert');
+
 describe('Homepage', () => {
   it('should show heading', async () => {
     await browser.url(`/`);
@@ -5,5 +7,26 @@ describe('Homepage', () => {
     await expect(
       $('h1=Find assured suppliers of digital social care records')
     ).toBeExisting();
+  });
+
+  it('can complete form and see results', async () => {
+    await browser.url(`/`);
+
+    await $('a=Filter suppliers').click();
+
+    await $('input[name="care-setting"][value="domiciliary"]').click();
+    await $('a=Continue').click();
+
+    await $('input[name="hardware"][value="no"]').click();
+    await $('a=Continue').click();
+
+    await $('input[name="capabilities"][value="work-offline"]').click();
+    await $('input[name="work-offline"][value="view-care-records"]').click();
+    await $('a=Continue').click();
+
+    const text = await $('h1').getText();
+
+    assert.ok(text.match(/^[0-9]+ of [0-9]+ suppliers match your criteria$/));
+
   });
 });
